ddf9962b7527ebcbcea2791dd38e308eacd07796
kingluo <dxz1uo@gmail.com>
2021-01-20 15:17:25 +0800

    deploy to Github Pages

---
+++ precache-manifest.f3a0685790773448a5542996cb760b7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ff07ff14b7105c231b2",
+    "url": "https://cdn.jsdelivr.net/gh/xyyxlq/xyyxlq.github.io@latest/css/app.e7fa2304.css"
+  },
+  {
+    "revision": "5ff07ff14b7105c231b2",
+    "url": "https://cdn.jsdelivr.net/gh/xyyxlq/xyyxlq.github.io@latest/js/app.76b2378b.js"
+  },
+  {
+    "revision": "2b9394184ebaa8581702",
+    "url": "https://cdn.jsdelivr.net/gh/xyyxlq/xyyxlq.github.io@latest/js/chunk-vendors.62decb99.js"
+  },
+  {
+    "revision": "3f3bc215e26af13e9ccf959608058f7e",
+    "url": "https://cdn.jsdelivr.net/gh/xyyxlq/xyyxlq.github.io@latest/img/avatar.3f3bc215.jpeg"
+  },
+  {
+    "revision": "ae8867a9f7422f65d4c787cf40f3c5a4",
+    "url": "https://cdn.jsdelivr.net/gh/xyyxlq/xyyxlq.github.io@latest/index.html"
+  },
+  {
+    "revision": "c52ec659c34011af18164baa5d1294a6",
+    "url": "https://cdn.jsdelivr.net/gh/xyyxlq/xyyxlq.github.io@latest/package.json"
+  },
+  {
+    "revision": "4c9a89414b97bb2053ccc7cb83c83b6e",
+    "url": "https://cdn.jsdelivr.net/gh/xyyxlq/xyyxlq.github.io@latest/busuanzi.pure.mini.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://cdn.jsdelivr.net/gh/xyyxlq/xyyxlq.github.io@latest/robots.txt"
+  }
+];
